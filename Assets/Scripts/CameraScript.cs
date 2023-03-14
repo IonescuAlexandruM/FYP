3076101040a89dbc0627510f5e7c312a7b71e9e6
Alexandru Ionescu <71647115+IonescuAlexandruM@users.noreply.github.com>
2023-03-14 15:32:57 +0000

    rule of thirsd now working on 4 buttons, forcing the player to select the right angle that helps them view the frame better than using the other angles

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -9,9 +9,10 @@ public class CameraScript : MonoBehaviour
     private CinemachineVirtualCamera cvCamera;
     private Cinemachine3rdPersonFollow c3dFollow;
 
-    [SerializeField] private Slider upDown;
-    [SerializeField] private Slider leftRight;
-    [SerializeField] private Slider forwardBackward;
+    [SerializeField] private Button behind;
+    [SerializeField] private Button above;
+    [SerializeField] private Button topRight;
+    [SerializeField] private Button botRight;
 
     // Start is called before the first frame update
     void Start()
@@ -19,49 +20,41 @@ public class CameraScript : MonoBehaviour
         cvCamera = GetComponent<CinemachineVirtualCamera>();
         c3dFollow = cvCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
 
-        UpDownValueChanged(PlayerPrefs.GetFloat("_upDown_"));
-        LeftRightValueChanged(PlayerPrefs.GetFloat("_leftRight_"));
-        ForwardBackwardValueChanged(PlayerPrefs.GetFloat("_forwardBackward_"));
+        ReloadCameraAngle(PlayerPrefs.GetFloat("_upDown_"), PlayerPrefs.GetFloat("_leftRight_"), PlayerPrefs.GetFloat("_forwardBackward_"));
+        
 
-        upDown.onValueChanged.AddListener(delegate { UpDownValueChanged(); });
-        leftRight.onValueChanged.AddListener(delegate { LeftRightValueChanged(); });
-        forwardBackward.onValueChanged.AddListener(delegate { ForwardBackwardValueChanged(); });
+        Button behindButton= behind.GetComponent<Button>();
+        behindButton.onClick.AddListener(()=>ChangeCameraPosition(0f, 0.5f, 0.5f)); 
+
+        Button aboveButton = above.GetComponent<Button>();
+        aboveButton.onClick.AddListener(() => ChangeCameraPosition(0.5f, 0.5f, 0.5f));
+
+        Button topRightButton = topRight.GetComponent<Button>();
+        topRightButton.onClick.AddListener(() => ChangeCameraPosition(0f, 0.8f, 1.5f));  
+        
+        Button botRightButton = botRight.GetComponent<Button>();
+        botRightButton.onClick.AddListener(() => ChangeCameraPosition(0f, 0.8f, 0.5f));
+
 
     }
 
-    void UpDownValueChanged()
+    void ReloadCameraAngle(float upDown, float leftRight, float forwardBackward)
     {
-        c3dFollow.ShoulderOffset.y = upDown.value;
-        PlayerPrefs.SetFloat("_upDown_", upDown.value);
-    } 
-    
-    void UpDownValueChanged(float value)
-    {
-        c3dFollow.ShoulderOffset.y = value;
-        upDown.value = value;
+        c3dFollow.ShoulderOffset.y = upDown;
+        c3dFollow.CameraSide = leftRight;
+        c3dFollow.CameraDistance = forwardBackward;
     }
 
-    void LeftRightValueChanged()
-    {
-        c3dFollow.CameraSide = leftRight.value;
-        PlayerPrefs.SetFloat("_leftRight_", leftRight.value);
-    }
-    
-    void LeftRightValueChanged(float value)
-    {
-        c3dFollow.CameraSide = value;
-        leftRight.value = value;
-    }
 
-    void ForwardBackwardValueChanged()
+    void ChangeCameraPosition(float upDown,float leftRight,float forwardBackward)
     {
-        c3dFollow.CameraDistance = forwardBackward.value;
-        PlayerPrefs.SetFloat("_forwardBackward_", forwardBackward.value);
-    }
-    
-    void ForwardBackwardValueChanged(float value)
-    {
-        c3dFollow.CameraDistance = value;
-        forwardBackward.value = value;
+        c3dFollow.ShoulderOffset.y = upDown;
+        PlayerPrefs.SetFloat("_upDown_", upDown);
+
+        c3dFollow.CameraSide = leftRight;
+        PlayerPrefs.SetFloat("_leftRight_", leftRight);
+
+        c3dFollow.CameraDistance = forwardBackward;
+        PlayerPrefs.SetFloat("_forwardBackward_", forwardBackward);
     }
 }
